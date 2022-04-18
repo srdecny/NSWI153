794476740896cc7e55a88e0b5908704fb2e7c445
Cross-bit <ondra.kryz@gmail.com>
2022-04-19 00:21:34 +0200

    add comments to 001-inti.sql

--- db/001-init.sql
+++ db/001-init.sql
@@ -1,3 +1,7 @@
+/*
+  Records table.
+*/
+
 CREATE TABLE record (
   id SERIAL PRIMARY KEY,
   url TEXT NOT NULL,
@@ -10,7 +14,9 @@ CREATE TABLE record (
 
 INSERT INTO record (url, boundary, periodicity, label, active, tags) VALUES ('http://www.example.com/', '/example.com/', 60, 'Example', TRUE, '');
 
-
+/*
+  Tags table.
+*/
 CREATE TABLE Tags (
   id SERIAL PRIMARY KEY,
   tag_name VARCHAR(64)
@@ -19,5 +25,9 @@ CREATE TABLE Tags (
 INSERT INTO Tags (tag_name) VALUES ('best');
 INSERT INTO Tags (tag_name) VALUES ('second');
 INSERT INTO Tags (tag_name) VALUES ('third');
+
+/*
+  Tags to records relation table.
+*/
 
 
